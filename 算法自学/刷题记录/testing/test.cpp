bd1bf20767fd386e55c683c5b8a146986d6c0297
donggengmo-ad <donggengmo@gmail.com>
2026-05-05 21:11:58 +0800

    2026.5.5

--- 算法自学/刷题记录/testing/test.cpp
+++ 算法自学/刷题记录/testing/test.cpp
@@ -1,25 +1,32 @@
 #include <iostream>
 #include <vector>
+#include <string>
 using namespace std;
-
-struct Node{
-    int data;
-    int parent;
-};
-int pow2(int p){
-    int res = 1, a = 2;
-    while(p > 0){
-        if(p & 1) res *= a;
-        a *= a;
-        p >>= 1;
+struct HT{
+    int len = 0, base = 31, cnt = 0;
+    vector<int> val;
+    int H(const string &s){
+        int res = 0;
+        for(char c:s) res = res * base + c;
+        return res % len;
     }
-    return res;
-}
-
+    HT(){}
+    HT(int l): len(l), val(l, 0){}
+    int& operator[](const string &s){
+        int i = H(s);
+        if(!val[i]) cnt++;
+        return val[i];
+    }
+};
 int main(){
     int n;
     cin >> n;
-    cout << pow2(n);
-    //vector<Node> a(pow2(n));
+    HT tb(n);
+    while(n--){
+        string s;
+        cin >> s;
+        tb[s]++;
+    }
+    cout << tb.cnt;
     return 0;
 }
