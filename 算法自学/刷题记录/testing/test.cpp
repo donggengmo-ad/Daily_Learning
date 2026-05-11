fc2c8dac93557209d7cfe79e6aa670c1929983bc
donggengmo-ad <donggengmo@gmail.com>
2026-05-11 23:21:35 +0800

    2026.5.11

--- 算法自学/刷题记录/testing/test.cpp
+++ 算法自学/刷题记录/testing/test.cpp
@@ -1,8 +1,18 @@
 #include <iostream>
-#include <vector>
-#include <list>
 using namespace std;
-
+using ll = long long;
 int main() {
+    int n;
+    cin >> n;
+    while(n--){
+        ll l, r;
+        cin >> l >> r;
+        ll ub = 0, lb = 0;
+        while(ub <= r) ub = (ub << 1) + 1, lb = (ub >> 1);
+        if(lb >= l) {cout << lb << endl; continue;}
+        ub = lb = l;
+        while(ub <= r) lb = ub, ub |= ub + 1;
+        cout << lb << endl;
+    }
     return 0;
 }
